6cdbdcc3fc36ae65225767c35f674f4e7e47ee62
Pablo Álamo <pablo.alamo@kairosds.com>
2022-10-24 11:55:41 +0200

    env

--- config/api.js
+++ config/api.js
@@ -1,6 +1,6 @@
 export default {
-  apiUrl: process.env.API_URL,
-  getWordPath: process.env.GET_WORD_PATH,
-  apiKey: process.env.API_KEY,
-  apiHost: process.env.API_HOST
+  apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  getWordPath: process.env.NEXT_PUBLIC_GET_WORD_PATH,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  apiHost: process.env.NEXT_PUBLIC_API_HOST
 };
